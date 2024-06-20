590a82f1025696d0da912099219aa1474b1db0c9
Matvey <m.baukov@bk.ru>
2024-06-20 23:50:54 +0400

    test telegram params

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  console.log("TelegramGameProxy", window.TelegramGameProxy.initParams);
   return (
     <div className="App">
       <header className="App-header">
